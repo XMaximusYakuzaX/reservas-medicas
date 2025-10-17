96cf09a208038e66bcd5f09fd059e183960b8f50
Angel Gabriel Carreon Trujillo <134726294+Anghel0082D@users.noreply.github.com>
2025-10-17 11:22:55 -0600

    feat(auth): add multi-factor authentication (MFA) (#82)
    * "docs: Multi-Factor Authentication and Session Management "
    
    * docs: add Multi-Factor Authentication and Session Management
    
    * feat: add Implementation of Multi-Factor Authentication and Zero Trust #53
    
    * commit styles: solve format
    
    * commit styles: solve format
    
    * commit styles: solve format
    
    closes #53

---
+++ src/screens/VerifyOTPScreen.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { auth } from '../auth/firebaseConfig';
+import { verifyOTP } from '../auth/mfaService';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigation/AppNavigator';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'VerifyOTP'>;
+
+export default function VerifyOTPScreen({ route, navigation }: Props) {
+  const { verificationId } = route.params;
+  const [code, setCode] = useState('');
+
+  const handleVerify = async () => {
+    try {
+      await verifyOTP(auth, verificationId, code);
+      Alert.alert('Éxito', 'Verificación completada');
+      navigation.replace('Home');
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        Alert.alert('Error', error.message);
+      } else {
+        Alert.alert('Error', 'Ocurrió un error desconocido durante la verificación.');
+      }
+    }
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', padding: 16 }}>
+      <Text style={{ fontSize: 20, marginBottom: 8 }}>Introduce el código OTP</Text>
+      <TextInput
+        style={{ borderWidth: 1, padding: 10, borderRadius: 5 }}
+        value={code}
+        onChangeText={setCode}
+        keyboardType="number-pad"
+      />
+      <Button title="Verificar código" onPress={handleVerify} />
+    </View>
+  );
+}
